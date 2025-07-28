1208b94613f95b2da4363abc2ae14c3b4486052d
Jaehyeong Park <qkrwogud0623@ajou.ac.kr>
2025-07-28 20:56:55 +0900

    fix: use HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { HashRouter as Router } from 'react-router-dom';
 
